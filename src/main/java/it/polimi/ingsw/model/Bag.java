13d87e1260b5f809d1b368a4b6c81ac571e114c3
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-04-04 14:23:02 +0200

    bag code and test (piu' o meno)

--- src/main/java/it/polimi/ingsw/model/Bag.java
+++ src/main/java/it/polimi/ingsw/model/Bag.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Bag {
     private Colour bag[];
 
-    private enum Colour{ GREEN, RED, YELLOW, PINK, BLUE };
+    private enum Colour{ GREEN, RED, YELLOW, PINK, BLUE ,NONE};
 
     public Bag(){
         this.bag = new Colour[] { Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,Colour.GREEN,
@@ -14,17 +14,38 @@ public class Bag {
                                   Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,Colour.PINK,
                                   Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE,Colour.BLUE
                                 };
-
-        private void randomize(){
-            Random r = new Random();
-            for(int i = 0; i < 130; i++){
-                int j = r.nextInt(131);
-                Colour tmp = bag[i];
-                bag[i] = bag[j];
-                bag[j] = tmp;
-            }
-        }
-
+        randomize();
     }
 
+    private void randomize(){
+        Random r = new Random();
+        for(int i = 0; i < 130; i++){
+            int j = r.nextInt(131);
+            Colour tmp = bag[i];
+            bag[i] = bag[j];
+            bag[j] = tmp;
+        }
+    }
+
+    public int extraction(){
+        int i = 0;
+        Colour tmp;
+
+        while(bag[i].equals(Colour.NONE) && i <= bag.length) i++;
+        tmp = bag[i];
+        bag[i] = Colour.NONE;
+
+        if(tmp.equals(Colour.GREEN)) return 0;
+        else if(tmp.equals(Colour.RED)) return 1;
+        else if(tmp.equals(Colour.YELLOW)) return 2;
+        else if(tmp.equals(Colour.PINK)) return 3;
+        else if(tmp.equals(Colour.BLUE)) return 4;
+
+        return -1;  //se ritorna -1 si e' cannato qualcosa!
+    }
+
+    public boolean checkVictory(){      //deve essere chiamato ogni volta che si fa un'estrazione
+        if(bag[bag.length].equals(Colour.NONE)) return true;
+        return false;
+    }
 }
---
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model;
+
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BagTest {
+    @Test
+    @DisplayName("First test")
+    public void testInitBag(){
+        Bag b1 = new Bag();
+
+
+    }
+
+}
