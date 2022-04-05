03bff2e6466e5b5effd2760b3c7f38beaa98e9f0
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-04-05 11:46:15 +0200

    BagTest

--- src/test/java/it/polimi/ingsw/model/BagTest.java
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -1,18 +1,25 @@
 package it.polimi.ingsw.model;
 
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagTest {
     @Test
     @DisplayName("First test")
-    public void testInitBag(){
+    void bagInit(){
         Bag b1 = new Bag();
+        int test;
 
-
+        test = b1.extraction();
+        Assertions.assertEquals(1,test);
     }
 
+
+
+    /*@Test
+    void extraction() {
+    }*/
 }
