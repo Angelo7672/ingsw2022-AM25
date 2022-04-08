ff83812ebf8f49d340728300d812951ffa1bfc59
chiaradipasquale <chiara2.dipasquale@mail.polimi.it>
2022-04-08 14:20:25 +0200

    Update pom

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,12 +1,21 @@
 package it.polimi.ingsw;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
      */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
 }
