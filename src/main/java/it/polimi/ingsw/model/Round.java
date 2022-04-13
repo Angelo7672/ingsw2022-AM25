d0fffecc9859acfffa4dafd4f44661893b13e0c8
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-04-13 15:28:27 +0200

    Round Fix

--- src/main/java/it/polimi/ingsw/model/Round.java
+++ src/main/java/it/polimi/ingsw/model/Round.java
@@ -4,12 +4,28 @@ public class Round {
     private CloudsManager cloudsManager;
     private IslandsManager islandsManager;
     private PlayerManager playerManager;
+    private Bag bag;
 
     public Round(int numberOfPlayer, String[] playersInfo){
+        this.bag = new Bag();
         this.cloudsManager = new CloudsManager(numberOfPlayer);
+        refreshStudentsCloud(numberOfPlayer);
         this.islandsManager = new IslandsManager();
         this.playerManager = new PlayerManager(numberOfPlayer, playersInfo);
     }
+
+    public void refreshStudentsCloud(int numberOfPlayer){
+
+        if(numberOfPlayer == 2 || numberOfPlayer ==4 ) {
+            for (int j = 0; j < numberOfPlayer; j++) {
+                for (int i = 0; i < 3; i++) {
+                    cloudsManager.refreshCloudStudents(bag.extraction(), j);
+                }
+            }
+        }else if(numberOfPlayer==3)
+            }
+        }
+
 
     public void moveStudent(int playerRef, int colour, boolean inSchool){
         if(!inSchool){
