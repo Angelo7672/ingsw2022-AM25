4b836e78ffd5c1c7d231865f8c2c38ac2e2eb1a4
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-04-19 18:40:11 +0200

    GameManager update

--- src/main/java/it/polimi/ingsw/model/CloudsManager.java
+++ src/main/java/it/polimi/ingsw/model/CloudsManager.java
@@ -2,56 +2,48 @@ package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
 
-public class CloudsManager /*implements GameSetup*/ {
-
+public class CloudsManager{
     private ArrayList<Cloud> clouds;
 
     private class Cloud{
         private int[] students;
-        private int studentNumber;
 
-        // Cloud constructor
-        public Cloud(int studentNumber){
-            this.studentNumber = studentNumber;
+        private Cloud(){
             this.students = new int[]{0, 0, 0, 0, 0};
         }
+
+        private int[] getStudents(){ return students; }
+        private int getColour(int colour){ return students[colour]; }
+        private void incrStudents(int colour){ students[colour]++; }
+        private void removeColour(int colour){ students[colour] = 0; }
     }
 
     // CloudsManager constructor, create an arrayList of Clouds.
     // Each cloud has 3 or 4 students on it, depending on the number of players */
     public CloudsManager(int playerNumber){
         clouds = new ArrayList<>();
-        int studentsNumber;
-
-        if(playerNumber == 3)
-            studentsNumber = 4;
-        else
-            studentsNumber = 3;
 
         for(int i = 0; i < playerNumber; i++){
-            Cloud cloud = new Cloud(studentsNumber);
+            Cloud cloud = new Cloud();
             clouds.add(cloud);
         }
     }
 
     // given the index of a cloud, return the array of students on that cloud
     public int[] getStudents(int cloudIndex) {
-        return clouds.get(cloudIndex).students;
+        return clouds.get(cloudIndex).getStudents();
     }
 
     //given the index of a cloud and the color of the student extracted from the bag, add the student to the cloud
-    public void refreshCloudStudents(int studentExtracted, int cloudIndex) {
-            clouds.get(cloudIndex).students[studentExtracted]++;
-    }
+    public void refreshCloudStudents(int studentExtracted, int cloudIndex) { clouds.get(cloudIndex).incrStudents(studentExtracted); }
 
     // given the index of a cloud, it empties the cloud and returns the array of the students on the cloud
     public int[] removeStudents(int cloudIndex) {
-        Cloud cloud = clouds.get(cloudIndex);
-        int[] students = new int[cloud.students.length];
+        int[] students = new int[5];
 
-        for (int i = 0; i < cloud.students.length; i++) {
-            students[i]=clouds.get(cloudIndex).students[i];
-            clouds.get(cloudIndex).students[i]=0;
+        for (int i = 0; i < 5; i++) {
+            students[i]=clouds.get(cloudIndex).getColour(i);
+            clouds.get(cloudIndex).removeColour(i);
         }
         return students;
     }
