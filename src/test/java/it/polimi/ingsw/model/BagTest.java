16f3db770245343ab944ebca9b216dc14cdec406
ginevra-bozza <ginevra.bozza@mail.polimi.it>
2022-06-14 00:40:27 +0200

    BagTest update

--- src/test/java/it/polimi/ingsw/model/BagTest.java
+++ src/test/java/it/polimi/ingsw/model/BagTest.java
@@ -9,15 +9,15 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-/*
+
 class BagTest {
     Bag bag;
 
-    
+
     @BeforeEach
-    void initialization(){
+    void initialization() {
         bag = new Bag();
-        BagListener bagListener = new BagListener() {
+        bag.bagListener = new BagListener() {
             @Override
             public void notifyBagExtraction() {
 
@@ -28,6 +28,7 @@ class BagTest {
 
             }
         };
+
         bag.bagInitialize();
 
 
@@ -35,10 +36,10 @@ class BagTest {
 
     @Test
     @DisplayName("First test: check if all 120 numbers drawn are compatible with the legend")
-    void bagInit(){
+    void bagInit() {
         int test;
 
-        for(int i = 0; i < 120; i++) {
+        for (int i = 0; i < 120; i++) {
             test = bag.extraction();
             assertTrue(test >= 0, "A number outside the legend has been extracted");
             assertTrue(test < 5, "A number outside the legend has been extracted");
@@ -60,3 +61,4 @@ class BagTest {
         assertTrue(bag.checkVictory(),"The bag be empty");
     }
 */
+}
