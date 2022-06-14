4399f2295acae5432d5a6e9ac4308fcb9646a00e
ginevra-bozza <ginevra.bozza@mail.polimi.it>
2022-06-14 12:38:56 +0200

    gui updates

--- src/main/java/it/polimi/ingsw/client/GUI/MainSceneController.java
+++ src/main/java/it/polimi/ingsw/client/GUI/MainSceneController.java
@@ -5,24 +5,23 @@ import it.polimi.ingsw.client.View;
 import it.polimi.ingsw.controller.listeners.*;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.ResourceBundle;
+import java.util.List;
 
 
-public class MainSceneController implements SceneController, TowersListener, ProfessorsListener,PlayedCardListener,
+public class MainSceneController<wizardImage> implements SceneController, TowersListener, ProfessorsListener,PlayedCardListener,
         MotherPositionListener, IslandListener, CoinsListener, StudentsListener, InhibitedListener {
 
     private GUI gui;
@@ -33,8 +32,16 @@ public class MainSceneController implements SceneController, TowersListener, Pro
     private Exit proxy;
     private int numberOfPlayers;
     private boolean expertMode;
-    private HashMap<String, String> userInfo;
+    private HashMap<Integer, String> nicknamesMap;
+    private HashMap<Integer, String> charactersMap;
+    private HashMap<Integer, String> teamsMap;
+    private HashMap<Integer, ImageView> charactersImageMap;
     private int gamePhase;
+
+    private final String WIZARD = "/graphics/character_wizard.png";
+    private final String WITCH = "/graphics/character_witch.png";
+    private final String SAMURAI = "/graphics/character_samurai.png";
+    private final String KING = "/graphics/character_king.png";
 
     @FXML private ImageView island1;
     @FXML private ImageView island2;
@@ -49,33 +56,49 @@ public class MainSceneController implements SceneController, TowersListener, Pro
     @FXML private ImageView island11;
     @FXML private ImageView island12;
 
-    @FXML private ImageView schoolBoard1;
-    @FXML private ImageView schoolBoard2;
-    @FXML private ImageView schoolBoard3;
-    @FXML private ImageView schoolBoard4;
-
     @FXML private Button useSpecialButton;
     @FXML private ImageView motherNature;
-    @FXML private ImageView character1;
     @FXML private AnchorPane islandsPane;
     @FXML private AnchorPane school1;
     @FXML private AnchorPane school2;
     @FXML private AnchorPane school3;
     @FXML private AnchorPane school4;
+
     @FXML private AnchorPane userInfo1;
     @FXML private AnchorPane userInfo2;
+    @FXML private AnchorPane userInfo3;
+    @FXML private AnchorPane userInfo4;
+
+    @FXML private ImageView character1;
+    @FXML private ImageView character2;
+    @FXML private ImageView character3;
+    @FXML private ImageView character4;
+
+
     @FXML private HBox player1Box;
     @FXML private HBox player2Box;
     @FXML private VBox player3Box;
     @FXML private VBox player4Box;
 
     public MainSceneController(){
-        this.userInfo=new HashMap<>();
+        this.nicknamesMap =new HashMap<>();
+        this.charactersMap =new HashMap<>();
+        this.charactersImageMap =new HashMap<>();
         this.numberOfPlayers=4;
         this.expertMode=false;
     }
 
-
+    /*
+    public void initialize(){
+        System.out.println("initializeMainScene");
+        Platform.runLater(()->{
+            setNumberOfPlayers(view.getNumberOfPlayers());
+            setExpertMode(view.getExpertMode());
+            for (int i = 0; i < view.getNumberOfPlayers(); i++)
+                setUserInfo(view.getNickname(i), view.getCharacter(i));
+            startMainScene();
+        });
+    }*/
     public void startMainScene() {
         Platform.runLater(()->{
             if (numberOfPlayers == 2) {
@@ -84,12 +107,22 @@ public class MainSceneController implements SceneController, TowersListener, Pro
             } else if (numberOfPlayers == 3)
                 player4Box.setVisible(false);
 
-            if (expertMode == false) {
-                useSpecialButton.setVisible(false);
-            }
+            useSpecialButton.setVisible(expertMode);
+            charactersImageMap.put(0, character1);
+            charactersImageMap.put(1, character2);
+            charactersImageMap.put(2, character3);
+            charactersImageMap.put(3, character4);
+
+            Image image;
             for(int i=0; i<numberOfPlayers; i++){
-                //userInfo.
+                //nickname= nicknamesMap.get(i);
+                charactersImageMap.get(i).setImage(characterToImage(charactersMap.get(i)));
+                
+                //image= new Image();
+                //charactersImageMap.get(i).set
+                
             }
+            /*
             System.out.println("startMainScene");
             try {
                 if(proxy.startPlanningPhase()) {
@@ -102,10 +135,28 @@ public class MainSceneController implements SceneController, TowersListener, Pro
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
-            }
+            }*/
         });
 
     }
+    public Image characterToImage(String characterName) {
+        Image image = null;
+        if (characterName.equalsIgnoreCase("WIZARD")) {
+            image = new Image(WIZARD);
+        } else if(characterName.equalsIgnoreCase( "WITCH")) {
+            image = new Image(WITCH);
+        } else if(characterName.equalsIgnoreCase("SAMURAI")) {
+            image = new Image(SAMURAI);
+        } else if (characterName.equalsIgnoreCase("KING")){
+            image = new Image(KING);
+        }
+        return image;
+    }/*
+    public void setNickname(String nickname, int playerRef){
+        if(playerRef==0){
+
+        }
+    }*/
 
     public void showCards(){
         //fa comparire la finestra con le carte
@@ -139,8 +190,9 @@ public class MainSceneController implements SceneController, TowersListener, Pro
         this.expertMode = expertMode;
     }
 
-    public void setUserInfo(String nickname, String character) {
-        userInfo.put(nickname, character);
+    public void setUserInfo(String nickname, String character, int playerRef ) {
+        nicknamesMap.put(playerRef,nickname);
+        charactersMap.put(playerRef, character);
     }
 
     @Override
