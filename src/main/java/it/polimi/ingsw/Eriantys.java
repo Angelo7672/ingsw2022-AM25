f4f88d9d21e2a4cbff0a659137a1d23028cb3601
Angelo Attivissimo <angelo.attivissimo@mail.polimi.it>
2022-06-21 01:12:28 +0200

    Eryantis add

---
+++ src/main/java/it/polimi/ingsw/Eriantys.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.client.CLI.CLI;
+import it.polimi.ingsw.client.GUI.GUI;
+import it.polimi.ingsw.server.Server;
+
+
+public class Eriantys {
+    public static void main(String[] args) {
+        //TODO:frasi di benvenuto
+        //TODO: scelta se cli o gui oppure server
+    }
+}
